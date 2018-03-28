6f82c20e447b316055ccbc389f8f1d6c194aca8e
Joe Baker <joesbaker@gmail.com>
2018-03-28 18:07:21 +0100

    Adding some packages

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
